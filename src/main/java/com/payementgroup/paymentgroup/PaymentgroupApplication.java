e7241b0c84e17cbeb00f82c0e2b6b4518b820d24
Vishal-43 <vishalsodmise@gmail.com>
2025-09-05 14:49:16 +0530

    initalize a project

---
+++ src/main/java/com/payementgroup/paymentgroup/PaymentgroupApplication.java
@@ -0,0 +1,13 @@
+package com.payementgroup.paymentgroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentgroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentgroupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/payementgroup/paymentgroup/PaymentgroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payementgroup.paymentgroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentgroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
